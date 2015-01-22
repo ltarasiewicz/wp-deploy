6b4df0a4a9f3f6deb91e276c5345f686933a6272
vagrant <vagrant@scotchbox>
2015-01-22 17:05:28 +0000

    Add the Sparkling theme

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,7 +3,7 @@
 ############################################
 
 set :stage, :staging
-set :stage_url, "http://wp-deploy.local"
+set :stage_url, "http://wp-deploy.staging"
 server "92.222.76.119", user: "admin", roles: %w{web app db}
 set :deploy_to, "/var/www/wp-deploy.staging"
 
